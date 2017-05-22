f4ee0e03d93baa07b70edb695aab324d19bd8603
Hang Yan <yanhangyhy@gmail.com>
2017-05-22 10:55:50 +0800

    Update

--- sh/clean.sh
+++ sh/clean.sh
@@ -2,8 +2,9 @@
 DATE=$(date +"%Y-%m-%d")
 DIR=~/Pictures/Fuck/$DATE
 mkdir -p $DIR
-mv ~/Desktop/*.png $DIR/ 
+mv ~/Desktop/*.png $DIR/
 
-
-
-
+DIR=~/Documents/Code/$DATE
+mkdir -p $DIR
+mv ~/*.go $DIR
+mv ~/*.py $DIR
