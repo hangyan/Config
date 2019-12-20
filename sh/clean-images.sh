2ebedff4dc3faeb8b60381530529fb0dbdfd09f2
Hang Yan <hangyan@alauda.io>
2019-12-20 16:48:14 +0800

    Update

--- sh/clean-images.sh
+++ sh/clean-images.sh
@@ -1,5 +1,7 @@
 #/usr/bin/env bash
 
-docker ps -a |grep Exited | awk '{print $1}' | xargs docker rm
+# docker ps -a |grep Exited | awk '{print $1}' | xargs docker rm
+
+docker ps -a |grep -v Up | awk '{print $1}' | xargs docker rm
 
 docker images | grep "<none>" |awk '{print $3}' | xargs docker rmi
