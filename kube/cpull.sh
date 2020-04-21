90d80668e2fec8726dae36e4ee2237cbd60746eb
Hang Yan <hangyan@alauda.io>
2020-04-22 00:26:51 +0800

    Update

--- kube/cpull.sh
+++ kube/cpull.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 IMAGE=$1
-
+img=""
 
 if [[ "$IMAGE" =~ ^quay\.io.* ]]; then
-    echo "--- Use qiniu mirror for quay.io"
+    echo "--> Use qiniu mirror for quay.io"
     img="quay-mirror.qiniu.com"${IMAGE:7}
-    echo "-- Pulling $img"
-    docker pull $img
-    exit
 
 fi
 
 
 if [[ "$IMAGE" =~ ^gcr\.io.* ]]; then
-    echo "-- Use aliyun mirror for gci.io"
+    echo "--> Use aliyun mirror for gci.io"
     img="registry.aliyuncs.com"${IMAGE:6}
-    echo "-- Pulling $img"
-    docker pull $img
-    exit
 fi
+
+
+echo "--> Pulling $img"
+docker pull $img
+echo "--> Tag image"
+docker tag $img $IMAGE
